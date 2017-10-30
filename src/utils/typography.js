0fdec5aa340cadb180e5f404ea25aa03fc43390c
M Gosschalk <monica.gosschalk@btinternet.com>
2017-10-30 16:31:24 +0000

    second commit

---
+++ src/utils/typography.js
@@ -0,0 +1,9 @@
+import Typography from "typography"
+
+//const typography = new Typography({ baseFontSize: '18px' })
+//import bootstrapTheme from "typography-theme-bootstrap"
+import lawtonTheme from "typography-theme-lawton"
+
+//const typography = new Typography(bootstrapTheme)
+const typography = new Typography(lawtonTheme)
+export default typography
